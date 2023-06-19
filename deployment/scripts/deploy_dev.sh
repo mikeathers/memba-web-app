ad9cf96c1362071526f1fc9b41ef1d2b977abdad
Mike Atherton <athers_05@hotmail.co.uk>
2023-06-19 12:30:43 +0100

    chore: adds overwrite options to unzip in ci pipeline

--- deployment/scripts/deploy_dev.sh
+++ deployment/scripts/deploy_dev.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cd deployment
 
 echo "--- 🚀 Unzipping the build..."
-unzip -q dev-build.zip
+unzip -q -o dev-build.zip
 
 echo "--- 🚀 Installing npm dependencies..."
 yarn install --frozen-lockfile
--- deployment/scripts/deploy_prod.sh
+++ deployment/scripts/deploy_prod.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cd deployment
 
 echo "--- 🚀 Unzipping the build..."
-unzip -q prod-build.zip
+unzip -q -o prod-build.zip
 
 echo "--- 🚀 Installing npm dependencies..."
 yarn install --frozen-lockfile
