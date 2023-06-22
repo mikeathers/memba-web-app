53847e0e393a632ec4a58fd056487117b48b03aa
Mike Atherton <athers_05@hotmail.co.uk>
2023-06-22 14:06:57 +0100

    feat: removes cloudfront invalidation

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ module.exports = {
   '**/*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `eslint . --fix ${filenames.join(' ')}`,
+    `tsc --project tsconfig.json --pretty --noEmit`,
   ],
   '**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`,
 }
